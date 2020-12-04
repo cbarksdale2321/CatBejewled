a9a0e2ea0c06a9046cfeb5aecdb5d470bd9c6589
AYogore <AYogore@users.noreply.github.com>
2020-12-04 15:05:35 -0600

    ScoreManager ready and set

---
+++ Assets/ScoreManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class ScoreManager : MonoBehaviour
+{
+    [SerializeField]
+    private TextMeshProUGUI scoreText;
+    private static int score;
+
+    void Start()
+    {
+        scoreText.SetText($"Score = {score}");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
