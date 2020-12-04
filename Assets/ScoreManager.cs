ae527c6b12c01b28233385053a2b7ebb871affb4
AYogore <AYogore@users.noreply.github.com>
2020-12-04 15:25:47 -0600

    score with multiplier working

--- Assets/ScoreManager.cs
+++ Assets/ScoreManager.cs
@@ -8,15 +8,28 @@ public class ScoreManager : MonoBehaviour
     [SerializeField]
     private TextMeshProUGUI scoreText;
     private static int score;
+    private static int bonusMultiplier;
 
     void Start()
     {
-        scoreText.SetText($"Score = {score}");
+        score = 0;
+        bonusMultiplier = 1;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        scoreText.SetText($"Score = {score}");
     }
+
+    public static void IncrementScore(int scoreValue)
+    {
+        score += scoreValue * bonusMultiplier;
+    }
+
+    public static void IncrementMultiplier()
+    {
+        bonusMultiplier++;
+    }
+
 }
