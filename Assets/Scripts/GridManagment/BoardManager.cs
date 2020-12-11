b1323267f580dc4f08de6fcb61a6c5bbd5e50d48
cbarksdale2321 <55416957+cbarksdale2321@users.noreply.github.com>
2020-12-11 14:13:20 -0600

    Fixed logic bugs and added functionality to game pieces

--- Assets/Scripts/GridManagment/BoardManager.cs
+++ Assets/Scripts/GridManagment/BoardManager.cs
@@ -23,7 +23,7 @@ public class BoardManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     private void Setup()
@@ -36,8 +36,17 @@ public class BoardManager : MonoBehaviour
                 GameObject backgroundTile = Instantiate(tilePrefab, tempPostition, Quaternion.identity) as GameObject;
                 backgroundTile.transform.parent = this.transform;
                 backgroundTile.name = "( " + x + ", " + y + " )";
-                
+
                 int peiceToUse = Random.Range(0, gamePeices.Length);
+                int maxIterations = 0;
+
+                while (MatchesAt(x, y, gamePeices[peiceToUse]) && maxIterations < 100)
+                {
+                    peiceToUse = Random.Range(0, gamePeices.Length);
+                    maxIterations++;
+                }
+                maxIterations = 0;
+
                 GameObject peice = Instantiate(gamePeices[peiceToUse], tempPostition, Quaternion.identity);
                 peice.transform.parent = this.transform;
                 peice.name = "( " + x + ", " + y + " )";
@@ -45,4 +54,64 @@ public class BoardManager : MonoBehaviour
             }
         }
     }
+
+    private bool MatchesAt(int column, int row, GameObject piece)
+    {
+        if (column > 1 && row > 1)
+        {
+            if (allShapes[column - 1, row].tag == piece.tag && allShapes[column - 2, row].tag == piece.tag)
+            {
+                return true;
+            }
+            if (allShapes[column, row - 1].tag == piece.tag && allShapes[column, row - 2].tag == piece.tag)
+            {
+                return true;
+            }
+        }
+        else if (column <= 1 || row <= 1)
+        {
+            if (row > 1)
+            {
+                if (allShapes[column, row - 1].tag == piece.tag && allShapes[column, row - 2].tag == piece.tag)
+                {
+                    return true;
+                }
+            }
+            if (column > 1)
+            {
+                if (allShapes[column - 1, row].tag == piece.tag && allShapes[column - 2, row].tag == piece.tag)
+                {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+
+    private void DestroyMatchAt(int column, int row)
+    {
+        if (allShapes[column,row].GetComponent<GamePeice>().isMatched)
+        {
+            Destroy(allShapes[column, row]);
+            allShapes[column, row] = null;
+        }
+    }
+
+    public void DestroyMatches()
+    {
+        for (int x = 0; x < width; x++)
+        {
+            for (int y = 0; y < height; y++)
+            {
+                if (allShapes[x,y] != null)
+                {
+                    DestroyMatchAt(x, y);
+                }
+            }
+        }
+
+    }
 }
+
+
--- Assets/Scripts/GridManagment/GamePeice.cs
+++ Assets/Scripts/GridManagment/GamePeice.cs
@@ -4,7 +4,10 @@ using UnityEngine;
 
 public class GamePeice : MonoBehaviour
 {
+    [Header("Board Options")]
     public int column, row, targetX, targetY;
+    public int previousColumn;
+    public int previousRow;
     public bool isMatched = false;
     private GameObject otherPiece;
     private BoardManager board;
@@ -12,6 +15,7 @@ public class GamePeice : MonoBehaviour
     private Vector2 finalTouchPosition;
     private Vector2 tempPosition;
     public float swipeAngle = 0f;
+    public float swipeResist = 1f;
 
     //Ok, So the temporary sounds might get annoying after a bit, My bad -Dj
     private float PieceResetTime = 1f;
@@ -24,9 +28,11 @@ public class GamePeice : MonoBehaviour
         targetY = (int)transform.position.y;
         row = targetY;
         column = targetX;
+        previousRow = row;
+        previousColumn = column;
 
     }
-    
+
     // Update is called once per frame
     void Update()
     {
@@ -38,7 +44,7 @@ public class GamePeice : MonoBehaviour
         }
         targetX = column;
         targetY = row;
-       
+
         if (Mathf.Abs(targetX - transform.position.x) > .1)
         {
             tempPosition = new Vector2(targetX, transform.position.y);
@@ -58,13 +64,36 @@ public class GamePeice : MonoBehaviour
         }
         else
         {
-           tempPosition = new Vector2(transform.position.x, targetY);
+            tempPosition = new Vector2(transform.position.x, targetY);
             transform.position = tempPosition;
             board.allShapes[column, row] = this.gameObject;
         }
-        
-        
 
+
+
+    }
+
+    public IEnumerator CheckMove()
+    {
+        yield return new WaitForSeconds(.5f);
+        if (otherPiece != null)
+        {
+            if (!isMatched && !otherPiece.GetComponent<GamePeice>().isMatched)
+            {
+                otherPiece.GetComponent<GamePeice>().row = row;
+                otherPiece.GetComponent<GamePeice>().column = column;
+                row = previousRow;
+                column = previousColumn;
+
+            }
+            else
+            {
+                board.DestroyMatches();
+            }
+            otherPiece = null;
+        }
+       
+        
     }
 
     private void OnMouseDown()
@@ -79,32 +108,36 @@ public class GamePeice : MonoBehaviour
 
     void CalcAngle()
     {
-        swipeAngle = Mathf.Atan2(finalTouchPosition.y - firstTouchPosition.y, finalTouchPosition.x - firstTouchPosition.x) * (180/Mathf.PI);
-        Debug.Log(swipeAngle);
-        MovePieces();
+        if (Mathf.Abs(finalTouchPosition.y - firstTouchPosition.y) > swipeResist || Mathf.Abs(finalTouchPosition.x - firstTouchPosition.x) > swipeResist)
+        {
+            swipeAngle = Mathf.Atan2(finalTouchPosition.y - firstTouchPosition.y, finalTouchPosition.x - firstTouchPosition.x) * (180 / Mathf.PI);
+
+            MovePieces();
+        }
+
 
         //This UndoMove might be in the wrong spot. It misfires if the 1st shape is incorrect.  
-        Invoke("UndoMove", PieceResetTime);
+        // Invoke("UndoMove", PieceResetTime);
 
     }
 
     void MovePieces()
     {
-        if (swipeAngle > -45 && swipeAngle <= 45 && column < (board.width))//right swipe
+        if (swipeAngle > -45 && swipeAngle <= 45 && column < (board.width - 1))//right swipe
         {
             otherPiece = board.allShapes[column + 1, row];
             otherPiece.GetComponent<GamePeice>().column -= 1;
             column += 1;
         }
-        else if (swipeAngle > 45 && swipeAngle <= 135 && row < board.height)//up swipe
+        else if (swipeAngle > 45 && swipeAngle <= 135 && row < board.height - 1)//up swipe
         {
-            
-        
-           otherPiece = board.allShapes[column, row + 1];
-           otherPiece.GetComponent<GamePeice>().row -= 1;
-           row += 1;
+
+
+            otherPiece = board.allShapes[column, row + 1];
+            otherPiece.GetComponent<GamePeice>().row -= 1;
+            row += 1;
         }
-        else if ((swipeAngle > 135 || swipeAngle <= -135 ) && column > 0)//left swipe
+        else if ((swipeAngle > 135 || swipeAngle <= -135) && column > 0)//left swipe
         {
 
 
@@ -118,6 +151,7 @@ public class GamePeice : MonoBehaviour
             otherPiece.GetComponent<GamePeice>().row += 1;
             row -= 1;
         }
+        StartCoroutine(CheckMove());
         FindObjectOfType<AudioManager>().Play("GamePieceMove");
 
 
@@ -126,6 +160,8 @@ public class GamePeice : MonoBehaviour
     /// <summary>
     /// Same as move but reversed. To use we can Invoke it and have a timer.
     /// </summary>
+    /// 
+    /*
     private void UndoMove()
     {
         if (isMatched ==false)
@@ -163,29 +199,39 @@ public class GamePeice : MonoBehaviour
         }
 
     }
+    */
 
     void FindMatches()
     {
-        if (column> 0 && column < board.width-1)
+        if (column > 0 && column < board.width - 1)
         {
             GameObject leftPiece1 = board.allShapes[column - 1, row];
             GameObject rightPiece1 = board.allShapes[column + 1, row];
-            if(leftPiece1.tag == this.gameObject.tag&& rightPiece1.tag == this.gameObject.tag)
+            if (leftPiece1 != null && rightPiece1 != null)
             {
-                leftPiece1.GetComponent<GamePeice>().isMatched = true;
-                rightPiece1.GetComponent<GamePeice>().isMatched = true;
-                isMatched = true;
+                if (leftPiece1.tag == this.gameObject.tag && rightPiece1.tag == this.gameObject.tag)
+                {
+                    leftPiece1.GetComponent<GamePeice>().isMatched = true;
+                    rightPiece1.GetComponent<GamePeice>().isMatched = true;
+                    isMatched = true;
+                }
             }
+
         }
         if (row > 0 && row < board.height - 1)
         {
-            GameObject upPiece1 = board.allShapes[column , row - 1];
-            GameObject downPiece1 = board.allShapes[column , row + 1];
-            if (upPiece1.tag == this.gameObject.tag && downPiece1.tag == this.gameObject.tag)
+            GameObject upPiece1 = board.allShapes[column, row - 1];
+            GameObject downPiece1 = board.allShapes[column, row + 1];
+            if (upPiece1 != null && downPiece1 != null)
             {
-                upPiece1.GetComponent<GamePeice>().isMatched = true;
-                downPiece1.GetComponent<GamePeice>().isMatched = true;
-                isMatched = true;
+
+
+                if (upPiece1.tag == this.gameObject.tag && downPiece1.tag == this.gameObject.tag)
+                {
+                    upPiece1.GetComponent<GamePeice>().isMatched = true;
+                    downPiece1.GetComponent<GamePeice>().isMatched = true;
+                    isMatched = true;
+                }
             }
         }
     }
